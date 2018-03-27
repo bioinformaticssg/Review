218805c60b5128d701fc9963e96df6ef84c11422
Stacey Borrego <staceyborrego@gmail.com>
2018-03-27 16:45:32 -0700

    fixed path

--- 02_fastqc_demo.sh
+++ 02_fastqc_demo.sh
@@ -15,8 +15,8 @@ module load fastqc/0.11.7
 DATA_FILENAMES_R1=/data/users/sborrego/BioinformaticsSG/Review/griffith_analysis_demo/data/HBR_data_R1_filenames.txt
 DATA_FILENAMES_R2=/data/users/sborrego/BioinformaticsSG/Review/griffith_analysis_demo/data/HBR_data_R2_filenames.txt
 
-QC_OUT_DIR=/data/users/sborrego/BioinformaticsSG/Review/griffith_analysis_demo/fastqc_out
-QC_HTML_DIR=/data/users/sborrego/BioinformaticsSG/Review/griffith_analysis_demo/fastqc_out/fastqc_html
+QC_OUT_DIR=/data/users/sborrego/BioinformaticsSG/Review/griffith_analysis_demo/fastqc
+QC_HTML_DIR=/data/users/sborrego/BioinformaticsSG/Review/griffith_analysis_demo/fastqc/fastqc_html
 HTML="fastqc_html"
 
 # Here we are performing a loop that will find each file in our data_filenames.txt file.
